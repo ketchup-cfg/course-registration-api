e84ccf8706018a573f223679d03b665b57f83429
Trevor Pierce <yeahtrevorpierce@gmail.com>
2025-03-21 14:36:59 -0500

    Downgrade SDK to .NET 8 for compatibility with xUnit

--- CourseRegistration.Api/Program.cs
+++ CourseRegistration.Api/Program.cs
@@ -6,23 +6,11 @@ using CourseRegistration.Api.Services.Abstractions;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
 builder.Services.AddDbContext<CourseRegistrationContext>(opt =>
     opt.UseInMemoryDatabase("CourseRegistration"));
-
 builder.Services.AddScoped<ICourseService, CourseService>();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 app.UseHttpsRedirection();
 app.MapControllers();
