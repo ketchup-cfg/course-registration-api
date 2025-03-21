8c605370e7e8d8ef585dc85ca8f7c7aba4b4cb37
Trevor Pierce <yeahtrevorpierce@gmail.com>
2025-03-21 10:05:42 -0500

    Add scaffolded courses controller

---
+++ CourseRegistration.Api/Controllers/CoursesController.cs
@@ -0,0 +1,103 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CourseRegistration.Api.Data;
+using CourseRegistration.Api.Data.Models;
+
+namespace CourseRegistration.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoursesController : ControllerBase
+    {
+        private readonly CourseRegistrationContext _context;
+
+        public CoursesController(CourseRegistrationContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Courses
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Course>>> GetCourses()
+        {
+            return await _context.Courses.ToListAsync();
+        }
+
+        // GET: api/Courses/5
+        [HttpGet("{id:long}")]
+        public async Task<ActionResult<Course>> GetCourse(long id)
+        {
+            var course = await _context.Courses.FindAsync(id);
+
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return course;
+        }
+
+        // PUT: api/Courses/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id:long}")]
+        public async Task<IActionResult> PutCourse(long id, Course course)
+        {
+            if (id != course.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(course).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CourseExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Courses
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Course>> PostCourse(Course course)
+        {
+            _context.Courses.Add(course);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCourse", new { id = course.Id }, course);
+        }
+
+        // DELETE: api/Courses/5
+        [HttpDelete("{id:long}")]
+        public async Task<IActionResult> DeleteCourse(long id)
+        {
+            var course = await _context.Courses.FindAsync(id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            _context.Courses.Remove(course);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CourseExists(long id)
+        {
+            return _context.Courses.Any(e => e.Id == id);
+        }
+    }
+}
