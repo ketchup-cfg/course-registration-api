4b433d5f6270c664fc85fb30043c569bd5aa363f
Trevor Pierce <yeahtrevorpierce@gmail.com>
2025-03-21 16:44:07 -0500

    Refactor MockCourseService to take in a list of courses to ensure we more flexibly test different scenarios

--- CourseRegistration.Tests/CoursesControllerTests.cs
+++ CourseRegistration.Tests/CoursesControllerTests.cs
@@ -1,4 +1,5 @@
 using CourseRegistration.Api.Controllers;
+using CourseRegistration.Api.Data.Models;
 using CourseRegistration.Tests.Mocks;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,16 +7,41 @@ namespace CourseRegistration.Tests;
 
 public class CoursesControllerTests
 {
+    private readonly List<Course> _courses =
+    [
+        new()
+        {
+            Id = 1,
+            Subject = "HIST",
+            CourseNumber = "101",
+            Description = "Just a test HIST 101 course",
+        },
+        new()
+        {
+            Id = 2,
+            Subject = "MATH",
+            CourseNumber = "102",
+            Description = "Just a test MATH 102 course",
+        },
+        new()
+        {
+            Id = 3,
+            Subject = "COMP",
+            CourseNumber = "201",
+            Description = "Just a test COMP 201 course",
+        }
+    ];
+
     [Fact]
     public void GetCourses_WithExistingCourses_ReturnsOk()
     {
         // Arrange
-        var mockService = new MockCourseService();
+        var mockService = new MockCourseService(_courses);
         var controller = new CoursesController(mockService);
-        
+
         // Act
         var result = controller.GetCourses();
-        
+
         // Assert
         Assert.IsType<OkObjectResult>(result.Result);
     }
--- CourseRegistration.Tests/Mocks/MockCourseService.cs
+++ CourseRegistration.Tests/Mocks/MockCourseService.cs
@@ -7,32 +7,9 @@ public class MockCourseService : ICourseService
 {
     private readonly List<Course> _courses;
 
-    public MockCourseService()
+    public MockCourseService(List<Course> courses)
     {
-        _courses =
-        [
-            new Course
-            {
-                Id = 1,
-                Subject = "HIST",
-                CourseNumber = "101",
-                Description = "Just a test HIST 101 course",
-            },
-            new Course
-            {
-                Id = 2,
-                Subject = "MATH",
-                CourseNumber = "102",
-                Description = "Just a test MATH 102 course",
-            },
-            new Course
-            {
-                Id = 3,
-                Subject = "COMP",
-                CourseNumber = "201",
-                Description = "Just a test COMP 201 course",
-            }
-        ];
+        _courses = courses;
     }
 
     public IEnumerable<Course?> GetAllCourses()
