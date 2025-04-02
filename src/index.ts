bfc886367d3cfe50b2b9ebf7dbc353778985f098
Trevor Pierce <yeahtrevorpierce@gmail.com>
2025-04-02 11:20:41 -0500

    Add JSON middleware

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get("/", (_req, res) => {
-  res.send("Hello World!");
+  res.json({ message: "Hello World!" });
 });
 
 app.listen(port, () => {
