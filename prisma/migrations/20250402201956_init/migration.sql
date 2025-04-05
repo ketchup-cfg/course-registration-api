c0939b46b906160f9e45b570e06cca0a0aa99aef
Trevor Pierce <yeahtrevorpierce@gmail.com>
2025-04-05 12:36:10 -0500

    Remove Prisma since it apparently no longer works...

--- prisma/migrations/20250402201956_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Course" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "courseNumber" TEXT NOT NULL,
-    "subject" TEXT NOT NULL,
-    "description" TEXT NOT NULL
-);
