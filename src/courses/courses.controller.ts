6946529d702a31880644f6a5f08ea25e1ead438c
Trevor Pierce <yeahtrevorpierce@gmail.com>
2025-04-29 10:12:16 -0500

    Update GET requests to /courses to return a list of courses

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,9 +1,31 @@
 import { Controller, Get } from '@nestjs/common';
 
+interface Course {
+  id: number;
+  subjectCode: string;
+  courseNumber: string;
+  description: string;
+}
+
+const courses = [
+  {
+    id: 1,
+    subjectCode: 'ENGL',
+    courseNumber: '101',
+    description: 'A test ENGL 101 course',
+  },
+  {
+    id: 2,
+    subjectCode: 'MATH',
+    courseNumber: '201',
+    description: 'A test MATH 201 course',
+  },
+];
+
 @Controller('courses')
 export class CoursesController {
   @Get()
-  findAll(): string {
-    return 'This action returns all courses';
+  findAll(): Course[] {
+    return courses;
   }
 }
